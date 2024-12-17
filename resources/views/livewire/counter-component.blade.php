9301de23a6c5d8551e6a1694464c75ff7852a649
Francisco Berroeta <fsberroeta@gmail.com>
2024-12-17 19:58:36 -0300

    feat: Add Livewire CounterComponent and update task view to include it

---
+++ resources/views/livewire/counter-component.blade.php
@@ -0,0 +1,7 @@
+<div>
+    <p>Hola esto es el livewire</p>
+    <p>El contador es {{$count}}</p>
+    <button wire:click="increment">Increment</button>
+    <button wire:click="decrement">Decrement</button>
+
+</div>
---
+++ resources/views/task.blade.php
@@ -0,0 +1,3 @@
+<div>
+    @livewire('CounterComponent')
+</div>
